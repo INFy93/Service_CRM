fd6f1cb92c830346f61bcf60ffc7687987a295ea
INFy93 <infy93@gmail.com>
2024-03-07 22:36:00 +0300

    add changing status

---
+++ resources/js/composables/orders/orders.js
@@ -0,0 +1,21 @@
+import { useToast } from "vue-toastification";
+import { router } from "@inertiajs/vue3";
+export default function useOrders() {
+
+    const toast = useToast();
+    const currentUser = window.User;
+    const newStatus = async (order_id, status_id, service) => {
+
+        const url = route('orders.change-status');
+        if (status_id === 6 && service !== currentUser.service_id)
+            return toast.error("Невозможно закрыть заказ: не твой сервис.");
+        await router.put(url, {
+            status: status_id,
+            order_id: order_id
+            }, {preserveState: true, preserveScroll: true, only: ['orders']} )
+    }
+
+    return {
+        newStatus
+    }
+}
