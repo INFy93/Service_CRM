a5d1e195a9a26503cf149090c98b40004ca0fdbf
INFy93 <infy93@gmail.com>
2024-03-07 11:09:54 +0300

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link type="image/x-icon" href="/storage/img/favicon.ico" rel="shortcut icon">
+    <link type="Image/x-icon" href="/storage/img/favicon.ico" rel="icon">
     <script>
         window.User = {
             id: {{ optional(auth()->user())->id }},
